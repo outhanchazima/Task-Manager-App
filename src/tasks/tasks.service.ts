5c6a265520dc38f3cf8303685b639da67091abc0
outhan254 <outhan254@gmail.com>
2020-05-20 12:43:29 +0300

    refactor to use create-task.dto

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuidv1 } from 'uuid';
+import { CreateTaskDto } from './dto/creat-task.dto';
 @Injectable()
 export class TasksService {
     private tasks: Task[] = [];
@@ -8,17 +9,19 @@ export class TasksService {
     getAllTasks(): Task[] {
         return this.tasks;
     }
-    
-    // retuns a single task
-    createTask (title: string, description: string): Task {
-         const task: Task = {
-             id: uuidv1(),
-             title,
-             description,
-             status:TaskStatus.OPEN,
-         };
 
-         this.tasks.push(task);
-         return task;
+    // retuns a single task
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+
+        const task: Task = {
+            id: uuidv1(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+
+        this.tasks.push(task);
+        return task;
     }
 }
